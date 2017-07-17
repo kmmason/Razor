e065b5ed5d0cd404a11f8f168e58a5edbf718f1a
Kevin Mason <kevin.mason@outlook.com>
2017-07-17 14:24:46 -0500

    Razor Listing 5-2

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Razor.Models;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person";
+            Category = "Watersports',
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
