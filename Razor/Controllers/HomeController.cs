2e6e8dce2d9dccbf259ccd3526b0c619b187277c
Kevin Mason <kevin.mason@outlook.com>
2017-07-18 13:17:34 -0500

    Razor Listing 5-20

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -34,5 +34,19 @@ namespace Razor.Controllers
             return View(myProduct);
         }
 
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M },
+                new Product {Name = "Lifejacket", Price = 48.95M },
+                new Product {Name = "Soccerball", Price = 19.50M },
+                new Product {Name = "Corner Flag", Price = 34.95M }
+            };
+
+            return View(array);
+
+        }
+
     }
 }
